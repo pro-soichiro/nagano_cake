8549e77759f78585243e314528b27b5ba0203fb6
pro-soichiro <dr.1987626@gmail.com>
2021-10-09 01:34:32 +0000

    [Update] Order_detailテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_011917) do
+ActiveRecord::Schema.define(version: 2021_10_09_012755) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -74,6 +74,16 @@ ActiveRecord::Schema.define(version: 2021_10_09_011917) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "price", null: false
+    t.integer "amount", null: false
+    t.integer "making_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
