287c2d3b229cf380b54b109c2fc94e14faeb52a7
pro-soichiro <dr.1987626@gmail.com>
2021-10-11 06:55:38 +0000

    [Update] 商品編集機能の実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,10 +13,20 @@ class Admin::ItemsController < ApplicationController
     @item.save
     redirect_to admin_items_path
   end
-  
+
   def show
     @item = Item.find(params[:id])
-    
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
   end
 
   private
