944dce28894985539166ee164bdd1af30c1c0637
pro-soichiro <dr.1987626@gmail.com>
2021-10-25 03:37:52 +0000

    [Update] 注文情報確認画面　完了

--- Gemfile
+++ Gemfile
@@ -63,9 +63,9 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
-gem 'pry-byebug'
-
-gem "enum_help"
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+gem 'pry-byebug'
+
+gem "enum_help"
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,41 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
   def new
+    @order = Order.new
+  end
+
+  def confirm
+    @cart_items = CartItem.all
+    @sum = 0
+    @order = Order.new(order_params)
+    case params[:order][:select_address]
+    when '0' then
+      # current_customerのお届け先
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    when '1' then
+      # 登録済み住所
+      # @order = Order.new(order_params)
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    when '2' then
+      # 新しいお届け先
+      # @order = Order.new(order_params)
+    end
+    # binding.pry
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method,
+                                  :postal_code,
+                                  :address,
+                                  :name)
   end
 
 end
