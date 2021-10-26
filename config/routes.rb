878ee1ba928d38a3fa31293aef53bf724b53e669
pro-soichiro <dr.1987626@gmail.com>
2021-10-26 01:00:01 +0000

    [Update] ルーティング整理　途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,47 @@
 Rails.application.routes.draw do
 
-  root 'public/homes#top'
-  get 'about' => 'public/homes#about'
 
   scope module: :public do
+    root 'homes#top'
+    get 'about' => 'homes#about'
+
     resources :items,only: [:index,:show]
 
-    resources :customers,only:[:edit,:update]
+    get 'customers/sign_up' => 'registrations#new'
+
+    get 'customers/sign_in' => 'sessions#new'
+
     get 'customers/my_page' => 'customers#show'
+    resources :customers,only:[:edit,:update]
     get 'customers/unsubscribe' => 'customers#unsubscribe'
     patch 'customers/withdraw' => 'customers#withdraw'
 
-    # get 'cart_items' => 'cart_items#index'
-    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :cart_items,only:[:index,:create,:update,:destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+    resources :orders,only: [:new,:create,:index,:show]
 
     resources :addresses,only:[:index,:create,:edit,:update,:destroy]
     post 'orders/confirm' => 'orders#confirm'
     get 'orders/complete' => 'orders#complete'
-    resources :orders,only: [:new,:create,:index,:show]
   end
+  post 'customers/sign_in' => 'devise/sessions#create' ,as: "customer_session"
 
   devise_for :customers
 
   namespace :admin do
-    get 'sign_in' => 'devise/sessions#new'
-    post 'sign_in' => 'devise/sessions#create'
-    delete 'sign_out' => 'devise/sessions#destroy'
+    get 'sign_in' => 'sessions#new'
 
     get '' => 'homes#top'
 
+    resources :items,only: [:index,:new,:create,:show,:edit,:update]
+
     resources :genres,only: [:index,:create,:edit,:update]
 
-    resources :items,only: [:index,:new,:create,:show,:edit,:update]
     resources :customers,only: [:index,:edit,:show,:update]
+
     resources :orders,only: [:index,:show,:update]
   end
+
+  devise_for :admins
 end
