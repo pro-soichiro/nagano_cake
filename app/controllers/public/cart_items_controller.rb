476e70f1c6bf352927da99c9ed426c01c0548771
pro-soichiro <dr.1987626@gmail.com>
2021-10-28 08:15:41 +0000

    [Update] 注文情報入力画面に進めない、またはリンクを表示しない

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,7 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @sum = 0
+    # binding.pry
   end
 
   def create
